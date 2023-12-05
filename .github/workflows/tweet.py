7040a3f01341bf419e6bbdd92bafb3ec6fb9e2bb
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2023-12-06 01:15:00 +0900

    [skip ci]

--- .github/workflows/tweet.py
+++ .github/workflows/tweet.py
@@ -10,7 +10,9 @@ session = requests_oauthlib.OAuth1Session(
     os.environ["X_ACCESS_TOKEN_SECRET"]
 )
 
-session.post(
+res = session.post(
     "https://api.twitter.com/2/tweets",
     json={"text": f"ブログを更新しました: {os.environ['TITLE']}\nhttps://blog.qmainconts.dev/articles/{os.environ['ID']}/"}
 )
+
+print(res.text)

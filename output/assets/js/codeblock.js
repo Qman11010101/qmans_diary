93b1bdff59751dc1af052bdf0aa0d053ee07c47b
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2024-11-25 11:20:50 +0000

    Automatically generated

--- output/assets/js/codeblock.js
+++ output/assets/js/codeblock.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const articleBody = document.getElementById("atcbody").children;
     for (let i = 0; i < articleBody.length; i++) {
         // DIVかつdata-filename属性を持つ場合、中のPREの前(一番先頭)にファイル名ブロックを追加し、divにcodeblock-marginクラスを付与
-        // 中のCODEのborder-radiusを0 20px 20pxにする
+        // クラスhljsを付加する
+        // 最後にhljs.highlight()する
         if (articleBody[i].tagName === "DIV" && articleBody[i].hasAttribute("data-filename")) {
             let filename = articleBody[i].getAttribute("data-filename");
             let filenameBlock = document.createElement("div");
             filenameBlock.className = "code-filename";
             filenameBlock.innerText = filename;
             articleBody[i].prepend(filenameBlock);
+
+            const codeBlock = articleBody[i].querySelector("code");
+            codeBlock.classList.add("hljs");
         }
     }
+
+    hljs.highlightAll();
 }, false);

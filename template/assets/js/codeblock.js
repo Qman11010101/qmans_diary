2e9853c61882c3ceaedfb8e8842b0832aee459f4
Qman11010101 <30486982+Qman11010101@users.noreply.github.com>
2024-11-25 19:15:15 +0900

    Update codeblock.js

--- template/assets/js/codeblock.js
+++ template/assets/js/codeblock.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const articleBody = document.getElementById("atcbody").children;
     for (let i = 0; i < articleBody.length; i++) {
         // DIVかつdata-filename属性を持つ場合、中のPREの前(一番先頭)にファイル名ブロックを追加し、divにcodeblock-marginクラスを付与
+        // また、中のcodeにhljsクラスを付与
         // 中のCODEのborder-radiusを0 20px 20pxにする
         if (articleBody[i].tagName === "DIV" && articleBody[i].hasAttribute("data-filename")) {
             let filename = articleBody[i].getAttribute("data-filename");
@@ -10,6 +11,9 @@ document.addEventListener("DOMContentLoaded", function () {
             filenameBlock.className = "code-filename";
             filenameBlock.innerText = filename;
             articleBody[i].prepend(filenameBlock);
+
+            let codeBlock = articleBody[i].querySelector("code");
+            codeBlock.classList.add("hljs");
         }
     }
 }, false);

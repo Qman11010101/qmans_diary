6e54c207ac9192202aa8b67da50ad5d3f4ffe3bf
キューマン・エノビクト <30486982+Qman11010101@users.noreply.github.com>
2025-07-03 23:31:11 +0900

    adjust to microblogen v2.0.0

---
+++ resources/static/assets/js/time_course.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const timeTxt = document.querySelector("#maincol > section:nth-child(1) > div.date").innerText;
+    const time = timeTxt.includes("更新") ? new Date(timeTxt.match(/\((.*?) 更新\)/)[1]) : new Date(timeTxt);
+    const now = new Date();
+    const diff = now - time;
+    const diffYear = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+
+    const tcw = document.getElementById("time-course-warning");
+    if (diffYear >= 1) {
+        tcw.style.display = "flex";
+        tcw.innerText = "この記事は最終更新から" + String(diffYear) + "年以上が経過しています。";
+    }
+}, false);
